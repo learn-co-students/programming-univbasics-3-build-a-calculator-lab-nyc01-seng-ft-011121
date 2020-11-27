10a0d5d9e9ad03b947a2b5facf3305df83089962
juliochavesnyc <juliochavesnyc@gmail.com>
2020-11-27 04:19:15 +0000

    Done.

--- lib/math.rb
+++ lib/math.rb
@@ -1,23 +1,35 @@
 def addition(num1, num2)
-
+  num2 + num1
 end
+
+addition(5, 4)
 
 def subtraction(num1, num2)
-
+  num1 - num2
 end
+
+subtraction(10, 5)
 
 def division(num1, num2)
-
+  num1 / num2
 end
+
+division(8, 2)
 
 def multiplication(num1, num2)
-
+  num1 * num2
 end
+
+multiplication(4, 2)
 
 def modulo(num1, num2)
-
+  num1 % num2
 end
+
+modulo(10, 3)
 
 def square_root(num)
-
+  Math.sqrt(num)
 end
+
+square_root(81)
