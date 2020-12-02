afb2ee8f074f462274b8cd93155e4f3aa2cee6dc
Linda-Monique Nyangoh Timoh Hendricks <linda.timoh@gmail.com>
2020-12-02 00:38:06 +0000

    Done.

--- lib/math.rb
+++ lib/math.rb
@@ -1,23 +1,36 @@
-def addition(num1, num2)
-
+def addition (num1, num2)
+    sum = num1 + num2
+    puts sum 
+    return sum 
 end
 
-def subtraction(num1, num2)
 
+def subtraction(num1, num2)
+   sum = num1 - num2
+   puts sum
+   return sum
 end
 
 def division(num1, num2)
-
+  sum = num1 / num2 
+  puts sum
+  return sum 
 end
 
 def multiplication(num1, num2)
-
+  sum = num1 * num2
+  puts sum
+  return sum 
 end
 
 def modulo(num1, num2)
-
+    sum = num1 % num2
+    puts sum  
+    return sum
 end
 
 def square_root(num)
-
+    sum = Math.sqrt(num) 
+    puts sum 
+    return sum
 end
---
+++ linda.rb
@@ -0,0 +1,8 @@
+def addition (num1, num2)
+  num2 = 3
+  num1 = 4
+  return sum 
+end
+
+addition 
+
