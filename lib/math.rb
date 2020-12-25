04a950df061e2ef83aa4d9b4bcafd79d3e1358b5
Jamie Lawrence-Hurt <jpham1109@gmail.com>
2020-12-25 23:01:31 +0000

    Done.

--- lib/math.rb
+++ lib/math.rb
@@ -1,29 +1,29 @@
 def addition(num1, num2)
-  sum = num1 + num2
-  sum
+  puts "addition got called with #{num1} and #{num2}."
+  num1 + num2
 end
 
 def subtraction(num1, num2)
-  sub = num1 - num2
-  sub
+  puts "substraction got called with #{num1} and #{num2}."
+  num1 - num2
 end
 
 def division(num1, num2)
-  div = num1 / num2
-  div
+  puts "division got called with #{num1} and #{num2}."
+  num1 / num2
 end
 
 def multiplication(num1, num2)
-  mult = num1 * num2
-  mult
+  puts "multiplication got called with #{num1} and #{num2}."
+  num1 * num2
 end
 
 def modulo(num1, num2)
-  mod = num1 % num2
-  mod
+  puts "modulo got called with #{num1} and #{num2}."
+  num1 % num2
 end
 
 def square_root(num)
-  square = Math.sqrt(num)
-  square
+  puts "square_root got called with #{num}."
+  Math.sqrt(num)
 end
