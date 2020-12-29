2dd6dbd4bc0e1781ab6be79a889f5081dc801bc5
Karris  Black <karris.b@gmail.com>
2020-12-29 22:32:45 +0000

    Done.

--- lib/math.rb
+++ lib/math.rb
@@ -1,33 +1,32 @@
 def addition(num1, num2)
-  puts num1.to_i + num2.to_i
-  (5+4)
+ num1.to_i + num2.to_i
+
   
 end
 
 def subtraction(num1, num2)
-  puts num1.to_i - num2.to_i
-  (10-5)
+   num1.to_i - num2.to_i
+  
 
 end
 
 def division(num1, num2)
-  puts num1.to_i / num2.to_i
-  (50/2)
+  num1.to_i / num2.to_i
+  
 end
 
 def multiplication(num1, num2)
-  puts num1.to_i ^ num2.to_i
-  (4*30)
+ num1.to_i * num2.to_i
+  
 end
 
 def modulo(num1, num2)
-  puts num1.to_i % num2.to_i
-  (34%5)
+   num1.to_i % num2.to_i
 end
 
 def square_root(num)
-  puts num.to_i () 
-  (81)
+   Math.sqrt (num)
+  
 
 end  
 
